6dc4ef73ed7fc678578539a55aaf27245ba1aad7
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2023-02-25 23:12:53 -0800

    first commit

---
+++ src/components/format/text-box.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let question: string = "there is no question";
+  let userInput: string = "slap some text here";
+</script>
+
+<!-- prevent default -->
+<form>
+  <p>{question}</p>
+  <input type="text" bind:value={userInput} />
+  <button type="submit">submit</button>
+</form>
